2a2143174905ecb5c4905d82c31a4215bb686907
Kelsey Staniec <kstaniec@uri.edu>
2026-04-30 13:05:56 -0400

    2nd modification of kmer_analyzer, contains inline comments

--- kmer_analyzer.py
+++ kmer_analyzer.py
@@ -1,70 +1,77 @@
 import sys
 
 def validate_sequence(sequence, k):
-    if len(sequence) < k:
+    if len(sequence) < k or k < 1: #stop if the sequence is shoter thean the kmer sizer or if the size is either 0 or negative
         return False
     for nucleotide in sequence:
-        if nucleotide in '1234567890':
+        if nucleotide in '1234567890': #stop if there is a number
             return False
+        if nucleotide not in{'A', 'C', 'T', 'G'}: #stop if there is a letter that's not A,C,T,G
+          return False
     return True
-
+          
 def update_kmer_count(kmer_data, kmer, next_char):
-    if kmer not in kmer_data:
-        kmer_data[kmer] = {'count': 1, 'next_chars': {}}
+    if kmer not in kmer_data: #check if the kmer is already in the dictionary
+        kmer_data[kmer] = {'count': 0, 'next_chars': {}} #add kmer to the dictionary 
+    kmer_data[kmer]['count'] += 1 #add 1 to the kmer count
     
-    kmer_data[kmer]['count'] += 1
-    
-    if next_char not in kmer_data[kmer]['next_chars']:
-        kmer_data[kmer]['next_chars'][next_char] = 0
-    kmer_data[kmer]['next_chars'][next_char] += 1
-
+    if next_char !='':
+      if next_char not in kmer_data[kmer]['next_chars']: #check if next_char is already in the dictionary for that particular kmer
+          kmer_data[kmer]['next_chars'][next_char] = 0 #create a spot for the next_char and fill with 0
+      kmer_data[kmer]['next_chars'][next_char] += 1 #add 1 to the next_char count
+      
     return kmer_data
 
 def count_kmers_with_context(sequence, k):
-    kmer_data = {}
+    kmer_data = {} #initialize dictionary
     
-    for i in range(len(sequence) - k):
-        kmer = sequence[i:i+k]
-        next_char = sequence[i+k]
+    for i in range(len(sequence) - (k-1)): #loop through every letter until you're k-1 from end of sequence. This ensures you get the last kmer
+        kmer = sequence[i:i+k] #kmer is defined as sequence from i to i+k (noninclusive)
+        if (i+k) < len(sequence): #make sure kmer doesn't go to end of sequence
+          next_char = sequence[i+k] #get next character
+        else:
+          next_char = '' #if kmer does go to end of seueunce, next_char is blank
         
-        kmer_data = update_kmer_count(kmer_data, kmer, next_char)
+        kmer_data = update_kmer_count(kmer_data, kmer, next_char) #add to dictionary
     
     return kmer_data
 
 
 def write_results_to_file(kmer_data, output_filename):
-    sorted_kmers = sorted(kmer_data.keys())
+    sorted_kmers = sorted(kmer_data.keys()) #sorts kmers in alaphabetical order
     
-    with open(output_filename, 'w') as f:
-        for kmer in sorted_kmers:
-            next_chars = kmer_data[kmer]['next_chars']
+    with open(output_filename, 'a') as f: #opens outputfile and will append to whatever is already in there
+        for kmer in sorted_kmers: #loop through kmer list
+            next_chars = kmer_data[kmer]['next_chars'] #Get the next characters for each kmer
             
+            #Create a string of the next characters instead of them being separate list items
             next_char_str = " ".join(
                 f"{char}:{freq}" 
-                for char, freq in sorted(next_chars.items())
+                for char, freq in sorted(next_chars.items()) #sort alphabetically
             )
             
-            f.write(f"{kmer} {next_char_str}\n")
+            f.write(f"{kmer} {next_char_str}\n") #in output file, write each kmer followed by its string of next characters and counts
 
 
 def main():
-    sequence_file = sys.argv[1]
-    k = int(sys.argv[2])
-    output_file = sys.argv[3]
+    sequence_file = sys.argv[1] #input file is first thing on command line
+    k = int(sys.argv[2]) #k number is second thing on command line
+    output_file = sys.argv[3] #output file name is third thing on command line
     
-    print(f"Reading sequences from {sequence_file}...")
+    print(f"Reading sequences from {sequence_file}...") #print to know process has started
 
-    with open(sequence_file, 'r') as f:
+    with open(sequence_file, 'r') as f: #open input file
         for sequence in f:
-            sequence = sequence.strip()
+            sequence = sequence.strip() #removes everything from each sequence except the string
 
-            if not validate_sequence(sequence, k):
+            if not validate_sequence(sequence, k): #runs through validate_sequence function and skips if it doesn't meet the criteria
                 print(f"  Warning: Skipping sequence")
-                continue
+                continue 
             
-            kmer_data = count_kmers_with_context(sequence, k) 
+            kmer_data = count_kmers_with_context(sequence, k)  #creates kmer_data dictionary
             
-            write_results_to_file(kmer_data, output_file)
+            write_results_to_file(kmer_data, output_file) #writes dictionary to output file
 
+#Run the script
 if __name__ == '__main__':
     main()
