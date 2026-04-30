b4bee4b589fc7bc696dae1eda62a56a81074f4a4
Kelsey Staniec <kstaniec@uri.edu>
2026-04-30 13:25:48 -0400

    3rd modification of kmer_analyzer, docstrings for each function included

--- kmer_analyzer.py
+++ kmer_analyzer.py
@@ -1,6 +1,15 @@
 import sys
 
 def validate_sequence(sequence, k):
+  """Validates input sequence to determine if it's DNA
+  
+  Parameters:
+    sequence(str): DNA sequence to be analyzed
+    k(int): length of kmer
+    
+  Output:
+    True/False for whether or not sequence meets the requirements
+  """
     if len(sequence) < k or k < 1: #stop if the sequence is shoter thean the kmer sizer or if the size is either 0 or negative
         return False
     for nucleotide in sequence:
@@ -11,6 +20,17 @@ def validate_sequence(sequence, k):
     return True
           
 def update_kmer_count(kmer_data, kmer, next_char):
+  """Creates dictionary for kmer data including number of occurences and next characters
+  
+  Parameters:
+    kmer_data(dict): dictionary for kmer data. Starts as empty
+    kmer(str): kmer of length k sliced from sequence
+    next_char(str): character in sequence directly after kmer
+  
+  Output:
+    kmer_data(dict): updated dictionary with all kmers, counts, and next characters with their counts
+  """
+  
     if kmer not in kmer_data: #check if the kmer is already in the dictionary
         kmer_data[kmer] = {'count': 0, 'next_chars': {}} #add kmer to the dictionary 
     kmer_data[kmer]['count'] += 1 #add 1 to the kmer count
@@ -23,6 +43,15 @@ def update_kmer_count(kmer_data, kmer, next_char):
     return kmer_data
 
 def count_kmers_with_context(sequence, k):
+  """Count kmers and next characters without them being explicitly given: find them in sequence
+  
+  Parameters:
+    sequence(str): validated DNA sequence
+    k(int): length of kmer
+    
+  Output:
+    kmer_data(dict): updated dictionary with all kmers, counts, and next characters with their counts
+  """
     kmer_data = {} #initialize dictionary
     
     for i in range(len(sequence) - (k-1)): #loop through every letter until you're k-1 from end of sequence. This ensures you get the last kmer
@@ -30,7 +59,7 @@ def count_kmers_with_context(sequence, k):
         if (i+k) < len(sequence): #make sure kmer doesn't go to end of sequence
           next_char = sequence[i+k] #get next character
         else:
-          next_char = '' #if kmer does go to end of seueunce, next_char is blank
+          next_char = '' #if kmer does go to end of sequence, next_char is blank
         
         kmer_data = update_kmer_count(kmer_data, kmer, next_char) #add to dictionary
     
@@ -38,6 +67,15 @@ def count_kmers_with_context(sequence, k):
 
 
 def write_results_to_file(kmer_data, output_filename):
+  """Takes kmer_data dictionary and puts it in an accessible output file
+  
+  Parameters:
+    kmer_data(dict): dictionary generated from count_kmers_with_context
+    output_filename(txt): text file that the results will be written into
+    
+  Output:
+    output_filename(txt):text file with results written in
+  """
     sorted_kmers = sorted(kmer_data.keys()) #sorts kmers in alaphabetical order
     
     with open(output_filename, 'a') as f: #opens outputfile and will append to whatever is already in there
@@ -54,6 +92,16 @@ def write_results_to_file(kmer_data, output_filename):
 
 
 def main():
+  """Takes a file of DNA sequences and outputs a file of all kmers and next characters
+  
+  Parameters: 1,2,3 on command line
+    sequence_file(txt): raw input file with DNA sequences
+    k(int): length of kmer
+    output_file(txt): empty file to write into
+    
+  Output:
+    output_file(txt): text file with results written in
+  """
     sequence_file = sys.argv[1] #input file is first thing on command line
     k = int(sys.argv[2]) #k number is second thing on command line
     output_file = sys.argv[3] #output file name is third thing on command line
